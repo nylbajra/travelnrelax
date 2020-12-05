910433ae813a673fa58997f11457b3ba69f53d66
Sunil <macbookpro@MacBooks-MacBook-Pro.local>
2020-12-05 12:40:14 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    $brand = 'Travel N Relax';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $brand;?></title>
+</head>
+<body>
+    <h1><?php echo $brand;?></h1>
+
+    <footer>
+        <p>Copyright &copy; <?php echo $brand;?></p>
+    </footer>
+</body>
+</html>
